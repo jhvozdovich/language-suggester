27162b8d7b069dd6ac2e5bef2fa76dd7dcbbf243
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-03-27 09:59:38 -0700

    add jquery branching for first question, update html and css

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,23 @@ $(document).ready(function() {
     $(".intro").hide();
     $(".quiz").show();
   })
+
+  $("form#responses").submit(function(event){
+    event.preventDefault();
+
+    var difficulty = $("input:radio[name=difficulty]:checked").val();
+
+    if ((difficulty === "easy")) {
+      $(".quiz").hide();
+      $("#python").show();
+    } else if ((difficulty === "medium")) {
+      $(".quiz").hide();
+      $("#javascript").show();
+    } else if ((difficulty === "hard")) {
+      $(".quiz").hide();
+      $("#csharp").show();
+    } else {
+      alert("Please complete the quiz!")
+    }
+  })  
 })
